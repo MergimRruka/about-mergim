fa3b8313e8454414806dbc3bd1e79bbe37d71c5b
Mergim Rruka <mergimrruka@gmail.com>
2020-09-17 17:02:19 +0200

    Updates

---
+++ precache-manifest.32b76b135590690edcf626745ea892da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a455a548798e7522f3351d599d7f4c1",
+    "url": "/MergimRruka/about-mergim/index.html"
+  },
+  {
+    "revision": "9c51c246d6bbd9949a0a",
+    "url": "/MergimRruka/about-mergim/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f9a7364d562ea27d20c",
+    "url": "/MergimRruka/about-mergim/static/js/2.4eef6bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MergimRruka/about-mergim/static/js/2.4eef6bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c51c246d6bbd9949a0a",
+    "url": "/MergimRruka/about-mergim/static/js/main.33a3e14b.chunk.js"
+  },
+  {
+    "revision": "a6b85097338720d3c903",
+    "url": "/MergimRruka/about-mergim/static/js/runtime-main.7fa66754.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MergimRruka/about-mergim/static/media/logo.5d5d9eef.svg"
+  }
+]);
